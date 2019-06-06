5c71571fc50d7150087e2e886bd812ad3d31ed4c
irwing gameros <irwingg468@adlersproductions.com>
2019-06-05 22:57:42 -0700

    status

---
+++ assertionsExample.js
@@ -0,0 +1,13 @@
+var expect = require("chai").expect,
+  foo = "bar",
+  drinks = { coffee: ["Guatemalan", "colombian", "hawaiian"] };
+
+expect(foo).to.be.a("string");
+expect(foo).to.equal("bar");
+expect(foo).to.have.lengthOf(3);
+expect(beverages)
+  .to.have.property("tea")
+  .with.lengthOf(3);
+expect(drinks).to.be.a("object");
+expect(drinks).to.not.be.null;
+expect(drinks.coffee).to.bea("string");
