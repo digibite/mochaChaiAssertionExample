1b63c7d1cdf5dfc67798da9ead122740f39f8004
irwing gameros <irwingg468@adlersproductions.com>
2019-06-05 23:07:01 -0700

    status

--- assertionsExample.js
+++ assertionsExample.js
@@ -1,13 +1,17 @@
-var expect = require("chai").expect,
+const expect = require("chai").expect,
   foo = "bar",
   drinks = { coffee: ["Guatemalan", "colombian", "hawaiian"] };
 
-expect(foo).to.be.a("string");
-expect(foo).to.equal("bar");
-expect(foo).to.have.lengthOf(3);
-expect(beverages)
-  .to.have.property("tea")
-  .with.lengthOf(3);
-expect(drinks).to.be.a("object");
-expect(drinks).to.not.be.null;
-expect(drinks.coffee).to.bea("string");
+describe("drinks tests", () => {
+  it("this is where the tests go...", () => {
+    expect(foo).to.be.a("string");
+    expect(foo).to.equal("bar");
+    expect(foo).to.have.lengthOf(3);
+    expect(beverages)
+      .to.have.property("tea")
+      .with.lengthOf(3);
+    expect(drinks).to.be.a("object");
+    expect(drinks).to.not.be.null;
+    expect(drinks.coffee).to.bea("string");
+  });
+});
